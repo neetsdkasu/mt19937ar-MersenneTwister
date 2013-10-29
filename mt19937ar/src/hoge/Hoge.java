a6b333917b746e8b0852cb89499dffedf80aab7a
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-10-29 21:05:28 +0900

    Change Character code (Shift_JIS -> UTF-8). Change Output to (StdOut -> test.out).

--- mt19937ar/src/hoge/Hoge.java
+++ mt19937ar/src/hoge/Hoge.java
@@ -1,31 +1,36 @@
 package hoge;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import mt19937ar.MersenneTwister;
 
 public class Hoge {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		MersenneTwister rand = new MersenneTwister(10);
-
+	public static void main(String[] args) throws IOException {
+		MersenneTwister rand = new MersenneTwister();
+//		PrintStream out = System.out;
+		PrintStream out = new PrintStream("test.out");
+		
+		// mt19937ar.out 比較用出力
 	    int i;
 	    long[] init= {0x123, 0x234, 0x345, 0x456};
-	    int length=4;
+	    int length = 4;
 	    rand.init_by_array(init, length);
 	    /* This is an example of initializing by an array.       */
 	    /* You may use init_genrand(seed) with any 32bit integer */
 	    /* as a seed for a simpler initialization                */
-	    System.out.println("1000 outputs of genrand_int32()");
-	    for (i=0; i < 1000; i++) {
-	      System.out.printf("%10d ", rand.genrand_int32());
-	      if (i%5==4) System.out.println();
+	    out.println("1000 outputs of genrand_int32()");
+	    for (i = 0; i < 1000; i++) {
+	      out.printf("%10d ", rand.genrand_int32());
+	      if (i % 5 == 4) out.println();
 	    }
-	    System.out.println("\n1000 outputs of genrand_real2()");
-	    for (i=0; i<1000; i++) {
-	      System.out.printf("%10.8f ", rand.genrand_real2());
-	      if (i%5==4) System.out.println();
+	    out.println("\n1000 outputs of genrand_real2()");
+	    for (i = 0; i < 1000; i++) {
+	      out.printf("%10.8f ", rand.genrand_real2());
+	      if (i % 5 == 4) out.println();
 	    }		
-		
 	}
 
 }
