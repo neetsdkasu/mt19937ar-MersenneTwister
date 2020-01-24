76469ff9c2e619b0da7224cc82bab5187668864a
Leonardone @ NEETSDKASU <neetsdkasu@gmail.com>
2020-01-25 07:19:04 +0900

    code-format Test.java
    fixed #5

--- mt19937ar/test/src/Test.java
+++ mt19937ar/test/src/Test.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.io.PrintStream;
 
@@ -9,29 +7,31 @@ public class Test {
 
 	public static void main(String[] args) throws IOException {
 		MersenneTwister rand = new MersenneTwister();
-//		PrintStream out = System.out;
+		// PrintStream out = System.out;
 		PrintStream out = new PrintStream("test.out");
-		
+
 		// mt19937ar.out
-	    int i;
-	    long[] init= {0x123, 0x234, 0x345, 0x456};
-	    int length = 4;
-	    rand.init_by_array(init, length);
-	    /* This is an example of initializing by an array.       */
-	    /* You may use init_genrand(seed) with any 32bit integer */
-	    /* as a seed for a simpler initialization                */
-	    out.println("1000 outputs of genrand_int32()");
-	    for (i = 0; i < 1000; i++) {
-	      out.printf("%10d ", rand.genrand_int32());
-	      if (i % 5 == 4) out.println();
-	    }
-	    out.println("\n1000 outputs of genrand_real2()");
-	    for (i = 0; i < 1000; i++) {
-	      out.printf("%10.8f ", rand.genrand_real2());
-	      if (i % 5 == 4) out.println();
-	    }
-	    
-	    out.close();
+		int i;
+		long[] init = { 0x123, 0x234, 0x345, 0x456 };
+		int length = 4;
+		rand.init_by_array(init, length);
+		/* This is an example of initializing by an array. */
+		/* You may use init_genrand(seed) with any 32bit integer */
+		/* as a seed for a simpler initialization */
+		out.println("1000 outputs of genrand_int32()");
+		for (i = 0; i < 1000; i++) {
+			out.printf("%10d ", rand.genrand_int32());
+			if (i % 5 == 4)
+				out.println();
+		}
+		out.println("\n1000 outputs of genrand_real2()");
+		for (i = 0; i < 1000; i++) {
+			out.printf("%10.8f ", rand.genrand_real2());
+			if (i % 5 == 4)
+				out.println();
+		}
+
+		out.close();
 	}
 
 }
