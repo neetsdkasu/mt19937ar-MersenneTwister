2cdaf193748a9c375c06a1c5599d6541ce385089
Leonardone @ NEETSDKASU <neetsdkasu@gmail.com>
2020-01-26 22:35:42 +0900

    change TestRandom (stdout->file)

--- mt19937ar/test/src/TestRandom.java
+++ mt19937ar/test/src/TestRandom.java
@@ -1,60 +1,66 @@
+import java.io.PrintStream;
+
 import mt19937ar.Random;
 
 public class TestRandom {
 
-	public static void main(String[] args) {
-		Random rand = new Random(12345L);
+	public static void main(String[] args) throws Exception {
 
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%d, ", rand.nextInt(100));
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf(rand.nextBoolean() + ", ");
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%f, ", rand.nextDouble());
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%f, ", rand.nextGaussian());
-			if (i % 10 == 9)
-				System.out.println();
-		}
+		try (PrintStream out = new PrintStream("test_random.out")) {
 
-		long[] init = { 0x123, 0x234, 0x345, 0x456 };
-		rand.setSeed(init);
+			Random rand = new Random(12345L);
 
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%d, ", rand.nextInt(100));
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf(rand.nextBoolean() + ", ");
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%f, ", rand.nextDouble());
-			if (i % 10 == 9)
-				System.out.println();
-		}
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%f, ", rand.nextGaussian());
-			if (i % 10 == 9)
-				System.out.println();
-		}
+			for (int i = 0; i < 100; i++) {
+				out.printf("%d, ", rand.nextInt(100));
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf(rand.nextBoolean() + ", ");
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf("%f, ", rand.nextDouble());
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf("%f, ", rand.nextGaussian());
+				if (i % 10 == 9)
+					out.println();
+			}
 
-		rand.setSeed(12345L);
-		for (int i = 0; i < 100; i++) {
-			System.out.printf("%d, ", rand.nextInt(100));
-			if (i % 10 == 9)
-				System.out.println();
+			long[] init = { 0x123, 0x234, 0x345, 0x456 };
+			rand.setSeed(init);
+
+			for (int i = 0; i < 100; i++) {
+				out.printf("%d, ", rand.nextInt(100));
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf(rand.nextBoolean() + ", ");
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf("%f, ", rand.nextDouble());
+				if (i % 10 == 9)
+					out.println();
+			}
+			for (int i = 0; i < 100; i++) {
+				out.printf("%f, ", rand.nextGaussian());
+				if (i % 10 == 9)
+					out.println();
+			}
+
+			rand.setSeed(12345L);
+			for (int i = 0; i < 100; i++) {
+				out.printf("%d, ", rand.nextInt(100));
+				if (i % 10 == 9)
+					out.println();
+			}
 		}
 	}
 
